8f5a51548cd05d1ceaa047684736a6977f874358
hwu <realwuhuidong@gmail.com>
2024-08-01 11:40:50 +0200

    executecmd

---
+++ execute_cmd.c
@@ -0,0 +1,19 @@
+#include "minishell.h"
+
+int init_pipe(int **p)
+{}
+
+int execute_cmd(t_c *c, t_d *d)
+{
+    int pipeid[MAXMM][2];
+    int exits;
+
+    if valid_check(c)
+    if (!init_pipe(pipeid))
+        return (0);
+    while (c)
+    {
+        
+        c = c->next;
+    }
+}
